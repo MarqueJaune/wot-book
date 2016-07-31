46c67a8a0a757a85e454357fe33fa5ad16457a52
MarqueJaune <dossantos.esteves@gmail.com>
2016-07-31 18:59:13 +0100

    Added slight modifications to blink.js, mostly deleted blank spaces/lines

--- chapter4-gpios/my-ch4/blink.js
+++ chapter4-gpios/my-ch4/blink.js
@@ -5,6 +5,8 @@ var Gpio = onoff.Gpio,
   //#B Initialize pin 4 to be an output pin
   led = new Gpio(4, 'out'),
   interval;
+
+
 //#C This interval will be called every 2 seconds
 interval = setInterval(function () {
   //#D Synchronously read the value of pin 4 and transform 1 to 0 or 0 to 1
@@ -14,6 +16,7 @@ interval = setInterval(function () {
     console.log("Changed LED state to: " + value);
   });
 }, 2000);
+
 
 //#F Listen to the event triggered on CTRL^C
 process.on('SIGINT', function () {
